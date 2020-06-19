29436022545dcdd938ff64352d9972d8a5210d33
Liam Allard <lallard13@gmail.com>
2020-06-19 10:53:18 -0700

    Work pleaseee

--- cats/src/main/java/com/revature/cats/models/User.java
+++ cats/src/main/java/com/revature/cats/models/User.java
@@ -47,7 +47,7 @@ public class User {
 //  
 //  public void setPassword(String password) {
 //    this.password  = password;
-  }
+//  }
 
   public void setUsername(String username) {
     this.username = username;
